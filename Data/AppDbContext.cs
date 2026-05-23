b82fa3851b659793db84f783ae80e1dc2a8f0f62
LMouchess <166089771+LMouchess@users.noreply.github.com>
2026-05-23 12:36:29 -0500

    correciones de localhost

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SwapFX.API.Entities;
+
+namespace SwapFX.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
